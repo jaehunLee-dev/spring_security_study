522734da945151053c22edb4cdcc6cc2cade4490
jaehunLee-dev <jaehunLee.dev@gmail.com>
2023-05-07 16:07:41 +0900

    feat: security 기본 기능 유저 별 접속 인가 설정

---
+++ src/main/java/com/cos/security1/config/SecurityConfig.java
@@ -0,0 +1,27 @@
+package com.cos.security1.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity  //spring security filter 가 spring filter chain 에 등록됩니다.
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.authorizeHttpRequests()
+                .requestMatchers("/user/**").authenticated()
+                .requestMatchers("/manager/**").hasAnyRole("ADMIN","MANAGER")
+                .requestMatchers("/admin/**").hasRole("ADMIN")
+                .anyRequest().permitAll()
+                .and()
+                .formLogin()
+                .loginPage("/login");
+        return http.build();
+    }
+}
